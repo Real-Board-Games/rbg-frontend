ba44f123a1f731c6b5700b3c4f000d0d8e131ced
Ivan MSI <arigatory@gmail.com>
2023-03-08 22:50:50 +0300

    create skeleton, add title

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/layout/app/App';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App/>);
