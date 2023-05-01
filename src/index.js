9051ce0970b70631ad25d2a64a8fb8f5cab4da6a
Ivan MSI <arigatory@gmail.com>
2023-05-01 04:52:58 +0300

    update to ts

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'react-hot-loader';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './app/layout/app/App';
-
-const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(<App/>);
