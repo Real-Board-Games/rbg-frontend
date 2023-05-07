41b5db405bb584cee9cdd88ed30efaa5d1df8450
Ivan MSI <arigatory@gmail.com>
2023-05-08 00:34:39 +0300

    add more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import ButtonPage from './pages/ButtonPage';
+import { useState } from 'react';
+import { Dropdown } from './components/Dropdown';
+
+export type Option = { label: string; value: string };
 
 function App() {
+  const [selection, setSelection] = useState<Option | null>(null);
+
+  const handleSelect = (option: Option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: 'The Color Red', value: 'red' },
+    { label: 'The Color Green', value: 'green' },
+    { label: 'A Shade of Blue', value: 'blue' },
+  ];
+
   return (
-    <div>
-      <ButtonPage />
+    <div className='flex'>
+    <Dropdown options={options} value={selection} onChange={handleSelect} />
+    <Dropdown options={options} value={selection} onChange={handleSelect} />
+    <Dropdown options={options} value={selection} onChange={handleSelect} />
+
+
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
-import type { FunctionComponent } from "react";
-import className from "classnames";
+import type { FunctionComponent } from 'react';
+import className from 'classnames';
 
 type ExcludeFromTuple<T extends unknown[], U> = {
   [K in keyof T]: T[K] extends U ? never : T[K];
@@ -7,8 +7,8 @@ type ExcludeFromTuple<T extends unknown[], U> = {
 
 type Exclusive<T extends PropertyKey[], U = unknown> = T[number] extends infer E
   ? E extends string
-    ? Record<E, U> & { [k in ExcludeFromTuple<T, E>]?: never }
-    : never
+  ? Record<E, U> & { [k in ExcludeFromTuple<T, E>]?: never }
+  : never
   : never & object;
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
@@ -16,7 +16,7 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
     outline?: boolean;
     rounded?: boolean;
   }> &
-  Exclusive<["primary", "secondary", "success", "warning", "danger"], boolean>;
+  Exclusive<['primary', 'secondary', 'success', 'warning', 'danger'], boolean>;
 
 const Button: FunctionComponent<ButtonProps> = ({
   children,
@@ -31,22 +31,22 @@ const Button: FunctionComponent<ButtonProps> = ({
 }) => {
   const classes = className(
     rest.className,
-    "flex gap-1 items-center px-3 py-1.5 border",
+    'flex gap-1 items-center px-3 py-1.5 border',
     {
-      "border-blue-500 bg-blue-500": primary,
-      "border-gray-900 bg-gray-900": secondary,
-      "border-green-500 bg-green-500": success,
-      "border-yellow-400 bg-yellow-400": warning,
-      "border-red-500 bg-red-500": danger,
-      "rounded-full": rounded,
-      "text-white":
+      'border-rbgblue bg-rbgblue': primary,
+      'border-gray-900 bg-gray-900': secondary,
+      'border-green-500 bg-green-500': success,
+      'border-yellow-400 bg-yellow-400': warning,
+      'border-red-500 bg-red-500': danger,
+      'rounded-full': rounded,
+      'text-white':
         !outline && (primary || secondary || success || warning || danger),
-      "bg-white": outline,
-      "text-blue-500": outline && primary,
-      "text-gray-500": outline && secondary,
-      "text-green-500": outline && success,
-      "text-yellow-500": outline && warning,
-      "text-red-500": outline && danger,
+      'bg-white': outline,
+      'text-blue-500': outline && primary,
+      'text-gray-500': outline && secondary,
+      'text-green-500': outline && success,
+      'text-yellow-500': outline && warning,
+      'text-red-500': outline && danger,
     }
   );
 
